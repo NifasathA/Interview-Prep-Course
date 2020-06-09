381a95265e5c9bb8e60e8a499129c01cb91546a5
Nifasath Afshan <nshanawa@andrew.cmu.edu>
2020-06-08 21:57:43 -0400

    Unique Triplets final

--- unique_triplets.py
+++ unique_triplets.py
@@ -1,29 +1,32 @@
+def remove_val(arr, val1, val2, val3):
+         arr.remove(val1)
+         arr.remove(val2)
+         arr.remove(val3)
+
 def find_unique_triplets(arr):
-    n = len(arr)
     arr.sort()
     for i in range (0, len(arr)-1):
             left = i + 1
             right = len(arr) - 1
             present = 0
             while (left < right):
-
                     if (arr[i] + arr[left] + arr[right] == 0):
                             print ("Unique triplet ",  arr[i] , arr[left] , arr[right])
                             present = 1
-                            arr.pop(right)
-                            arr.pop(left)
-                            arr.pop(i)
+                            remove_val(arr,  arr[i] , arr[left] , arr[right])
+                            break
                     elif (arr[i] + arr[left] + arr[right] > 0):
                             right -= 1
                     else:
                             left += 1
                     if (present == 1):
-                            left = 5
-                            right = 0
+                            break
 
 
 def main():
         arr = list (map (int, input().split()))
+        while arr:
+                print("No input")
         find_unique_triplets(arr)
 
 if __name__=="__main__": 
